6c82b35dbc8d61ea4e9efa3eb1fa15e14d416ba6
Nisanur Sakar <snisanurirmak@mail.com>
2026-01-25 18:25:05 +0300

    HomeScreen.tsx dosyası temeli

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return (
+  return  <HomeScreen />; (
     <SafeAreaProvider>
       <AppNavigator />
     </SafeAreaProvider>
