91d505d61daebcf183bab0c29a24bde6e9908fef
Nisanur Sakar <snisanurirmak@mail.com>
2026-01-28 17:49:44 +0300

    File problems solved

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
-import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return  <HomeScreen />; (
+  return (
     <SafeAreaProvider>
       <AppNavigator />
     </SafeAreaProvider>
